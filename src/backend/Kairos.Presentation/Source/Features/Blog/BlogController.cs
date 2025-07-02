fc90298752a2f05fd475c08f8f1906e3ad8bccf8
Adyllson <domingosady11son@gmail.com>
2025-07-02 21:22:09 +0100

    Manutenção parte 24

--- src/backend/Kairos.Presentation/Source/Features/Blog/BlogController.cs
+++ src/backend/Kairos.Presentation/Source/Features/Blog/BlogController.cs
@@ -5,129 +5,161 @@ namespace Kairos.Presentation.Source.Features.Blog;
 public class BlogController(IBlogService service) : ControllerBase
 {
     #region List
-        [HttpGet("ListBlog")]
-        [EndpointSummary("Listando todos os posts do blog")]
-        public async Task<ActionResult> ListBlog([FromQuery] GetBlogsCommand command, CancellationToken token) 
-        { 
-            try
-            {
-                var response = await service.GetHandler(command, token);
-                Logger.LogToFile("ListBlog - Success", "Listando todos os posts do blog.");
-                return Ok(response);
-            }
-            catch(Exception error)
-            {
-                Logger.LogToFile("ListBlog - Error", $"Error {error.Message}");
-                return Problem($"Error: {error.Message}");
-            }
+    [HttpGet("ListBlog")]
+    [EndpointSummary("Listando todos os posts do blog")]
+    public async Task<ActionResult> ListBlog([FromQuery] GetBlogsCommand command, CancellationToken token)
+    {
+        try
+        {
+            var response = await service.GetHandler(command, token);
+            Logger.LogToFile("ListBlog - Success", "Listando todos os posts do blog.");
+            return Ok(response);
         }
+        catch (Exception error)
+        {
+            Logger.LogToFile("ListBlog - Error", $"Error {error.Message}");
+            return Problem($"Error: {error.Message}");
+        }
+    }
     #endregion
 
     #region GetById
-        [HttpGet("GetByIdBlog")]
-        [EndpointSummary("Retorna um post específico pelo ID")]
-        public async Task<ActionResult> GetByIdBlog([FromQuery] GetBlogByIdCommand command, CancellationToken token) 
-        { 
-            try
-            {
-                var response = await service.GetByIdHandler(command, token);
-                Logger.LogToFile("GetByIdBlog - Success", "Retorna um post específico pelo ID");
-                return Ok(response);
-            }
-            catch(Exception error)
-            {
-                Logger.LogToFile("GetByIdBlog, - Error", $"Error: {error.Message}");
-                return Problem($"Error: {error.Message}");
-            }
+    [HttpGet("GetByIdBlog")]
+    [EndpointSummary("Retorna um post específico pelo ID")]
+    public async Task<ActionResult> GetByIdBlog([FromQuery] GetBlogByIdCommand command, CancellationToken token)
+    {
+        try
+        {
+            var response = await service.GetByIdHandler(command, token);
+            Logger.LogToFile("GetByIdBlog - Success", "Retorna um post específico pelo ID");
+            return Ok(response);
         }
+        catch (Exception error)
+        {
+            Logger.LogToFile("GetByIdBlog, - Error", $"Error: {error.Message}");
+            return Problem($"Error: {error.Message}");
+        }
+    }
     #endregion
 
     #region GetImage
-        [HttpGet("GetImageBlog")]
-        [EndpointSummary("Retorna imagem específico pelo ID")]
-        public async Task<ActionResult> GetImageBlog([FromQuery] GetFileBlogCommand command, CancellationToken token) 
-        { 
-            try
-            {
-                var response = await service.GetFileHandler(command, token);
-                Logger.LogToFile("GetImageBlog - Success", "Retorna imagem específico pelo ID");
-                return Ok(response);
-            }
-            catch(Exception error)
-            {
-                Logger.LogToFile("GetImageBlog - Error", $"Error: {error.Message}");
-                return Problem($"Error: {error.Message}");
-            }
-        }
-    #endregion
-    
-    #region GetPublish
-        [HttpGet("GetPublishBlog")]
-        [EndpointSummary("Retorna apenas posts publicados")]
-        public async Task<ActionResult> GetPublishBlog([FromQuery] GetPublishBlogCommand command, CancellationToken token) 
+    [HttpGet("GetImageBlog")]
+    [EndpointSummary("Retorna imagem específico pelo ID")]
+    public async Task<ActionResult> GetImageBlog([FromQuery] GetFileBlogCommand command, CancellationToken token)
+    {
+        try
         {
-            var response = await service.GetPublishHandler(command, token);
+            var response = await service.GetFileHandler(command, token);
+            Logger.LogToFile("GetImageBlog - Success", "Retorna imagem específico pelo ID");
             return Ok(response);
         }
+        catch (Exception error)
+        {
+            Logger.LogToFile("GetImageBlog - Error", $"Error: {error.Message}");
+            return Problem($"Error: {error.Message}");
+        }
+    }
+    #endregion
+
+    #region GetPublish
+    [HttpGet("GetPublishBlog")]
+    [EndpointSummary("Retorna apenas posts publicados")]
+    public async Task<ActionResult> GetPublishBlog([FromQuery] GetPublishBlogCommand command, CancellationToken token)
+    {
+        var response = await service.GetPublishHandler(command, token);
+        return Ok(response);
+    }
     #endregion
 
     #region Search
-        [HttpGet("SearchBlog")]
-        [EndpointSummary("Pesquisa posts por critérios definidos")]
-        public async Task<ActionResult> SearchBlog([FromQuery] SearchBlogCommand command, CancellationToken token) 
-        {
-            var response = await service.SearchHendler(command, token);
-            return Ok(response);
-        }
+    [HttpGet("SearchBlog")]
+    [EndpointSummary("Pesquisa posts por critérios definidos")]
+    public async Task<ActionResult> SearchBlog([FromQuery] SearchBlogCommand command, CancellationToken token)
+    {
+        var response = await service.SearchHendler(command, token);
+        return Ok(response);
+    }
     #endregion
 
     #region Create
-        [HttpPost("CreateBlog")]
-        [EndpointSummary("Cria um novo post no blog")]
-        public async Task<ActionResult> CreateBlog(CreateBlogCommand command, CancellationToken token)
-        {
-            var response = await service.CreateHandler(command, token);
-            return Ok(response);
-        }
+    [HttpPost("CreateBlog")]
+    [EndpointSummary("Cria um novo post no blog")]
+    public async Task<ActionResult> CreateBlog([FromForm] BlogCreateModel model, CancellationToken token)
+    {
+        if (model.ImagemCapaUrl == null || model.ImagemCapaUrl.Length == 0)
+            {
+                return BadRequest("Nenhuma imagem foi enviada.");
+            }
+
+            string pastaRaiz = "Storage";
+            string pastaImagens = Path.Combine(pastaRaiz, "Images");
+            if (!Directory.Exists(pastaImagens))
+            {
+                Directory.CreateDirectory(pastaImagens);
+            }
+
+            var extensao = Path.GetExtension(model.ImagemCapaUrl.FileName).ToLower();
+            var extensoesPermitidas = new HashSet<string> { ".jpg", ".jpeg", ".png", ".gif" };
+            if (!extensoesPermitidas.Contains(extensao))
+            {
+                return BadRequest($"Extensão de arquivo não suportada: {extensao}. Permitidos: JPG, JPEG, PNG, GIF.");
+            }
+
+            string nomeArquivo = $"{Guid.NewGuid()}{extensao}";
+            string caminhoCompleto = Path.Combine(pastaImagens, nomeArquivo);
+
+            await using var stream = new FileStream(caminhoCompleto, FileMode.Create);
+            await model.ImagemCapaUrl.CopyToAsync(stream);
+
+            var newCommand = new CreateBlogCommand{
+                UsuarioID = model.UsuarioID,
+                Titulo = model.Titulo,
+                Conteudo = model.Conteudo,
+                ImagemCapaUrl = caminhoCompleto
+            };
+        var response = await service.CreateHandler(newCommand, token);
+        return Ok(response);
+    }
     #endregion
 
     #region Edit
-        [HttpPut("EditBlog")]
-        [EndpointSummary("Edita completamente um post existente")]
-        public async Task<ActionResult> EditBlog(UpdateBlogCommand command, CancellationToken token) { 
-            var response = await service.UpdateHendler(command, token);
-            return Ok(response);
-        }
+    [HttpPut("EditBlog")]
+    [EndpointSummary("Edita completamente um post existente")]
+    public async Task<ActionResult> EditBlog(UpdateBlogCommand command, CancellationToken token)
+    {
+        var response = await service.UpdateHendler(command, token);
+        return Ok(response);
+    }
     #endregion
 
     #region Delete
-        [HttpDelete("DeleteBlog")]
-        [EndpointSummary("Remove um post do blog pelo ID")]
-        public async Task<ActionResult> DeleteBlog([FromQuery] DeleteBlogCommand command, CancellationToken token) 
-        {
-            var response = await service.DeleteHandler(command, token);
-            return Ok(response);
-        }
+    [HttpDelete("DeleteBlog")]
+    [EndpointSummary("Remove um post do blog pelo ID")]
+    public async Task<ActionResult> DeleteBlog([FromQuery] DeleteBlogCommand command, CancellationToken token)
+    {
+        var response = await service.DeleteHandler(command, token);
+        return Ok(response);
+    }
     #endregion
 
     #region Archive
-        [HttpPatch("ArchiveBlog")]
-        [EndpointSummary("Arquiva um post do blog")]
-        public async Task<ActionResult> ArchiveBlog(ArchiveBlogCommand command, CancellationToken token) 
-        { 
-            var response = await service.ArchiveHandler(command, token);
-            return Ok(response);
-        }
+    [HttpPatch("ArchiveBlog")]
+    [EndpointSummary("Arquiva um post do blog")]
+    public async Task<ActionResult> ArchiveBlog(ArchiveBlogCommand command, CancellationToken token)
+    {
+        var response = await service.ArchiveHandler(command, token);
+        return Ok(response);
+    }
     #endregion
 
     #region Publish
-        [HttpPatch("PublishBlog")]
-        [EndpointSummary("Publicar um post do blog para visivel")]
-        public async Task<ActionResult> PublishBlog(PublishBlogCommand command, CancellationToken token) 
-        { 
-            var response = await service.PublishHandler(command, token);
-            return Ok(response);
-        }
+    [HttpPatch("PublishBlog")]
+    [EndpointSummary("Publicar um post do blog para visivel")]
+    public async Task<ActionResult> PublishBlog(PublishBlogCommand command, CancellationToken token)
+    {
+        var response = await service.PublishHandler(command, token);
+        return Ok(response);
+    }
     #endregion
 }
 
